ef93b360fdcb4c9969c1d173f6387c41b274e0bc
newtondotcom <asphalt9fr@gmail.com>
2026-02-04 11:26:29 +0100

    chore: update repo structure to use only one branch for multiple packages

---
+++ ros2-interfaces-jazzy/tests/test_publisher.rs
@@ -0,0 +1,33 @@
+use ros2_client::{Context, MessageTypeName, Name, NodeName, NodeOptions};
+use ros2_interfaces_rolling::std_msgs;
+
+#[test]
+#[cfg(feature = "std_msgs")]
+fn test_publisher() {
+    let context = Context::new().unwrap();
+    let mut node = context
+        .new_node(
+            NodeName::new("/rustdds", "rustdds_listener").unwrap(),
+            NodeOptions::new().enable_rosout(true),
+        )
+        .unwrap();
+
+    let topic = node
+        .create_topic(
+            &Name::new("/","topic").unwrap(),
+            MessageTypeName::new("std_msgs", "String"),
+            &ros2_client::DEFAULT_PUBLISHER_QOS,
+        )
+        .unwrap();
+
+    let publisher = node
+        .create_publisher::<std_msgs::msg::String>(&topic, None)
+        .unwrap();
+
+
+    let message = std_msgs::msg::String {
+        data: "Hello, world!".to_string(),
+    };
+
+    publisher.publish(message).unwrap();
+}
